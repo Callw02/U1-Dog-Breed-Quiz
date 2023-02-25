97a96c542abfd31fc69a9813d5739f079c1fcd9e
Callw02 <muficcalle@gmail.com>
2023-02-25 17:12:56 +0100

    Done

--- JS/login_register.js
+++ JS/login_register.js
@@ -1,33 +1,39 @@
 "use strict"
 
-document.getElementById("register_link").addEventListener("click", register_page)
+document.getElementById("register_link").addEventListener("click", register_page);
+document.querySelector("#wrapper").classList.add("login_page");
+
 
 
 if(localStorage.getItem("quiz_html")){
-    document.querySelector("body").innerHTML = localStorage.getItem("quiz_html")
-    
+    document.querySelector("body").innerHTML = localStorage.getItem("quiz_html");
 }
 function register_page(event){
     
-    let wrapper = document.querySelector("#wrapper")
-    wrapper.classList.toggle("registration")
+    let wrapper = document.querySelector("#wrapper");
+    wrapper.classList.toggle("registration");
+    wrapper.classList.add("transition");
+    document.querySelector("#username_input").value = "";
+    document.querySelector("#password_input").value = "";
 
-    if(wrapper.classList.contains("registration")){
-    
-    document.querySelector("h1").textContent = "Register"
-    document.querySelector("p").textContent = "Ready when you are..."
-    document.querySelector("p").classList.remove("login_failed")
-    document.querySelector("button").textContent = "Register"
-    document.querySelector("#register_link p").textContent = "Already have an account? Go to login"
+    if(wrapper.classList.contains("registration")){    
+    document.querySelector("#wrapper").classList.add("registration");
+    document.querySelector("#wrapper").classList.remove("login_page");
+    document.querySelector("h1").textContent = "Register";
+    document.querySelector("p").textContent = "Ready when you are...";
+    document.querySelector("p").classList.remove("login_failed");
+    document.querySelector("button").textContent = "Register";
+    document.querySelector("#register_link p").textContent = "Already have an account? Go to login";
     }
     else{
-        document.querySelector("h1").textContent = "Login"
-        document.querySelector("p").textContent = "Let the magic start!"
-        document.querySelector("button").textContent = "Login"
-        document.querySelector("#register_link p").textContent = "New to this? Register For free"
+        document.querySelector("#wrapper").classList.add("login_page");
+        document.querySelector("h1").textContent = "Login";
+        document.querySelector("p").textContent = "Let the magic start!";
+        document.querySelector("button").textContent = "Login";
+        document.querySelector("#register_link p").textContent = "New to this? Register For free";
     }        
 }
-    document.querySelector("button").addEventListener("click", login_or_reg)
+    document.querySelector("button").addEventListener("click", login_or_reg);
 
     
 async function login_or_reg(event){
@@ -49,45 +55,46 @@ async function login_or_reg(event){
    
     if (event.target.textContent === "Register") {
 
-       let register_rqst = new Request (`https://teaching.maumt.se/apis/access/`, options)
-       register_function(register_rqst)   
+       let register_rqst = new Request (`https://teaching.maumt.se/apis/access/`, options);
+       register_function(register_rqst);
     }
 
     if(event.target.textContent === "Login"){
-        let login_rqst = new Request (`https://teaching.maumt.se/apis/access/?action=check_credentials&user_name=${user}&password=${pass}`)
-        login_function(login_rqst)
+        let login_rqst = new Request (`https://teaching.maumt.se/apis/access/?action=check_credentials&user_name=${user}&password=${pass}`);
+        login_function(login_rqst);
     }
     
 }
 
 async function register_function(register_request){
-    
     let div = document.createElement("div");
-    
+    div.classList.add("connecting");
     
     show_feedback(div);
 
-    let resource = await fetch_function(register_request)
+    let resource = await fetch_function(register_request);
     
     switch (resource.status) {
         case 418:
-            div.textContent = "The server thinks it's not a teapot!"
+            div.textContent = "The server thinks it's not a teapot!";
             break;
         case 409:
-            div.textContent = "Sorry that name is taken. Please try with another one"
+            div.textContent = "Sorry that name is taken. Please try with another one";
         break;
         case 400:
-            div.textContent = "Bad request"
+            div.textContent = "Sorry that name is taken. Please try with another one";
             break;
-        default: div.textContent = "Registration Complete. Please proceed to login"
+        default: div.textContent = "Registration Complete. Please proceed to login";
         break;
     }
-   
-    let button = document.createElement("button")
-    button.textContent = "CLOSE"
-    button.classList.add("close_btn")
-    div.appendChild(button)
-    button.addEventListener("click", close_feedback)
+    div.classList.remove("connecting");
+    div.classList.add("info_div");
+    let button = document.createElement("button");
+    button.textContent = "CLOSE";
+    button.classList.add("close_btn");
+    div.appendChild(button);
+    button.addEventListener("click", close_feedback);
+    
 }
 
 async function login_function(login_request){
@@ -95,30 +102,31 @@ async function login_function(login_request){
     let pass = document.querySelector("#password_input").value;
    
     let div = document.createElement("div");
-    div.classList.add("connecting")
+    div.classList.add("connecting");
+
         show_feedback(div)
         let resource = await fetch_function(login_request);
         switch (resource.status) {
                 case 418:
-                    div.textContent = "The server thinks it's not a teapot!"
-                    div.classList.remove("connecting")
-                    let button = document.createElement("button")
-                    button.textContent = "CLOSE"
-                    button.classList.add("close_btn")
-                    div.appendChild(button)
-                    button.addEventListener("click", close_feedback)
+                    div.textContent = "The server thinks it's not a teapot!";
+                    div.classList.remove("connecting");
+                    let button = document.createElement("button");
+                    button.textContent = "CLOSE";
+                    button.classList.add("close_btn");
+                    div.appendChild(button);
+                    button.addEventListener("click", close_feedback);
                 break;
         
             default:
-                document.querySelector("#wrapper").classList.remove("registration_info")
-                let p = document.querySelector("p")
-                p.classList.add("login_failed")
-                p.textContent = "Wrong username or password."   
-                div.remove()
+                document.querySelector("#wrapper").classList.remove("registration_info");
+                let p = document.querySelector("p");
+                p.classList.add("login_failed");
+                p.textContent = "Wrong username or password.";
+                div.remove();
                 break;
         }
        if(resource.data.password === pass && resource.data.user_name === user){
-        successful_login(resource.data.user_name) 
+        successful_login(resource.data.user_name);
        }
 }
 
@@ -126,25 +134,24 @@ async function login_function(login_request){
 
 function show_feedback(div){
         let wrapper = document.querySelector("#wrapper");
-        wrapper.classList.add("registration_info");
         div.classList.add("info_div");
+        wrapper.classList.add("registration_info");
         div.textContent = "Contacting server...";
         document.querySelector("body").appendChild(div);
 }
 
 function close_feedback(event){
-    let div = document.querySelector(".info_div")
-    
-    div.remove()
+    let div = document.querySelector(".info_div");
+    div.remove();
     let wrapper = document.querySelector("#wrapper");
         wrapper.classList.remove("registration_info");
 }
 
 function button_function(button){
     let div = document.createElement("div");
-    button.textContent = "CLOSE"
-    button.classList.add("close_btn")
-    div.appendChild(button)
-    button.addEventListener("click", close_feedback)
+    button.textContent = "CLOSE";
+    button.classList.add("close_btn");
+    div.appendChild(button);
+    button.addEventListener("click", close_feedback);
 }
 
--- JS/quiz.js
+++ JS/quiz.js
@@ -1,6 +1,6 @@
 "use strict"
 async function successful_login(name){
-    let body = document.querySelector("body")
+    let body = document.querySelector("body");
     body.innerHTML = `
     <body>
     <div id="wrapper">
@@ -20,16 +20,20 @@ async function successful_login(name){
     <footer> <p>The Dog Breed Quiz is made possible thanks to the free api by <span>DOG CEO Zine</span></p> </footer>
 
 </div>`
-get_dog();
-
-document.querySelector("#logout_button").addEventListener("click", logout_function);
-localStorage.setItem("quiz_html", body.innerHTML);
+   
+    document.querySelector("#wrapper").classList.add("quiz_page");
+    document.querySelector("#logout_button").addEventListener("click", logout_function);
+    localStorage.setItem("quiz_html", body.innerHTML);
+    get_dog()
 }
 
+
 document.querySelector("#logout_button").addEventListener("click", logout_function);
 
+get_dog();
+
 function logout_function(event){
-    localStorage.removeItem("quiz_html")
+    localStorage.removeItem("quiz_html");
     document.querySelector("body").innerHTML = `
     <div id="wrapper">
     <div id="logo_title">
@@ -39,12 +43,12 @@ function logout_function(event){
 
     <h1>Login</h1>
     <div id="login">
-        <div class="input">
+        <div class="input_field">
             <h3>User Name:</h3>
             <input id="username_input" type="text">
         </div>
 
-        <div class="input">
+        <div class="input_field">
             <h3>Password:</h3>
             <input id="password_input" type="password">
         </div>
@@ -68,25 +72,39 @@ function logout_function(event){
     <script src="index.js"></script>`
     document.getElementById("register_link").addEventListener("click", register_page);
     document.querySelector("button").addEventListener("click", login_or_reg);
+    document.querySelector("#wrapper").classList.add("login_page")
 }
 
-get_dog()
+
 
 
 async function get_dog(){
+    let wrapper = document.querySelector("#wrapper");
+    wrapper.classList.add("registration_info");
+    let div = document.createElement("div");
+    div.classList.add("info_div");
+    div.style.height = "60px";
+    wrapper.appendChild(div);
+    div.textContent = "Getting a random dog";
+
     let image_div = document.querySelector("#dog_image");
     let img = document.createElement("img");
     img.classList.add("dog_image");
-
+    
     let index = get_number(ALL_BREEDS.length);
     let random_dog = ALL_BREEDS[index].url;
     let dog_request = new Request(`https://dog.ceo/api/breed/${random_dog}/images/random`);
     let resource = await fetch_function(dog_request);
+    div.remove();
+    
+    img.src = "media/logo.png";
     img.src = resource.message;
-    image_div.appendChild(img);
 
+    image_div.appendChild(img);
+    wrapper.classList.remove("registration_info");
     make_options(4, index);
 }
+
 
 function get_number(length){
     return Math.floor(Math.random() * length);
@@ -109,9 +127,9 @@ function choices(number, dog){
         let index = get_number(ALL_BREEDS.length);
         let dog_name = ALL_BREEDS[index].name;
         console.log(dog_name);
-        let button = document.querySelector(`#button_id_${i}`)
+        let button = document.querySelector(`#button_id_${i}`);
         button.textContent = dog_name;
-        button.addEventListener("click", check_answer)
+        button.addEventListener("click", check_answer);
     }
     let correct_answer = document.querySelector(`#button_id_${number}`);
     correct_answer.setAttribute("id", "correct");
@@ -121,28 +139,31 @@ function choices(number, dog){
 
 
 function check_answer(event){
-    document.querySelector("#wrapper").classList.add("registration_info")
-    let div = document.createElement("div")
-    div.classList.add("answer")
-    document.querySelector("body").appendChild(div)
+    document.querySelector("#wrapper").classList.add("registration_info");
+    let div = document.createElement("div");
+    div.classList.add("answer");
+    document.querySelector("body").appendChild(div);
 
     
     if(event.target.id === "correct"){
-        div.style.backgroundColor = "lime"
-        div.textContent = "CORRECT!"
+        div.style.backgroundColor = "lime";
+        div.textContent = "CORRECT!";
         
     }else{
-        div.style.backgroundColor = "orange"
-        div.textContent = "I'm afraid not..:("
+        div.style.backgroundColor = "orange";
+        div.textContent = "I'm afraid not..:(";
     }
-    let button = document.createElement("button")
-    button.textContent = "ONE MORE"
-    button.classList.add("close_btn")
+
+    let button = document.createElement("button");
+    button.textContent = "ONE MORE";
+    button.classList.add("close_btn");
     div.append(button);
     
-    button.addEventListener("click", new_question)
+    button.addEventListener("click", new_question);
 }
+
+
 function new_question(event){
-    let name = document.querySelector("#user_information > p").textContent
-    successful_login(name)
+    let name = document.querySelector("#user_information > p").textContent;
+    successful_login(name);
 }
